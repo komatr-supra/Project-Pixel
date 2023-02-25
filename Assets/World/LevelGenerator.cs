a122c7e691518c081bcfd660318413a1ba957263
komatr-supra <rudolf.bouzek@seznam.cz>
2023-02-25 13:19:36 +0100

    generator, parallax
    level generator is complete for now,
    parallax effect is under construction

--- Assets/World/LevelGenerator.cs
+++ Assets/World/LevelGenerator.cs
@@ -1,3 +1,11 @@
+/*
+This is script for generating random platform level.
+Generating rects with tile data (ChunkData contains TileLevelSO and RectInt).
+So i dont need check every grid position. 
+Props and also enemy is from TileLevelSO and callculated for each chunk.
+Just between chunks -> can be handled transition(between tiles), slopes.
+At the end are chunks set to the right tilemap.
+*/
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,64 +16,82 @@ namespace Generator
     public class LevelGenerator : MonoBehaviour
     {
         #region Struct of terrain data
-        //maybe can be switch to scriptable object
-        
-        
         private struct ChunkData
         {
             public TileLevelSO TileSO;
             public RectInt rect;
         }
         #endregion
-        #region Variables
-        [SerializeField] private Spawner spawner;        
+        #region Variables   
+        [Header("Platforms")]        
+        [Tooltip("Lenght of starting platform, player will start in the middle.")]
+        [Range(10, 50)]
+        [SerializeField] private int startPlatformLenght = 20;
+        [Range(0, 100)]
+        [Tooltip("Minimal tile height")]
+        [SerializeField] private int minTileHeight = 2;
+        [Range(0, 100)]
+        [Tooltip("Maximal tile height")]
+        [SerializeField] private int maxTileHeight = 8;
+        [Range(0, 100)]
+        [Tooltip("Maximal height change between platforms")]
+        [SerializeField] private int maxTileHeightChange = 2;
+        [Space]
+        [Header("Map")]
+        [Tooltip("X is horizontal size of the map, Y is the vertical size of map")]
+        [SerializeField] private Vector2Int mapSize;
+        [Tooltip("Tile setup Scriptable Object for this map.")]
+        [SerializeField] private TileLevelSO[] tileSO;
+        [Space]
+        [Header("System - SETUP!")]
+        [SerializeField] GameObject propsGO;
         [SerializeField] private Tilemap collisionTilemap;
         [SerializeField] private Tilemap backgroundTilemap;
-        [SerializeField] private int startChunkLenght = 20;
-        [SerializeField] private int minTileHeight = 2;
-        [SerializeField] private int maxTileHeight = 8;
-        [SerializeField] private int maxTileHeightChange = 2;
-        [SerializeField] private TileLevelSO[] chunksForThisMap;
-        [SerializeField] private Vector2Int mapSize;
-        [SerializeField] GameObject propsGO;
         private int mapLenght = 0;
-        //first chunk of terrain must be walkable
+        //first terrain chunk must be walkable
         private bool mustBeWalkable = true;
         #endregion
         private void Awake()
         {
-            Debug.Log("awake creating");
+            //generate main data
             List<ChunkData> mapChunks = GenerateMapFrontChunksData();
-            //generate background tiles
-            GenerateBackgroundTiles(mapChunks);
+            //update background data(here is just adjust height -> dont overlap main terrain)
+            UpdateBackgroundData(mapChunks);
             //generate platforms
 
-            //generate props
+            //generate visual props
             GenerateProps(mapChunks);
             //update array to tilemap
-            UpdateTilesToTilemap(mapChunks);
-            
+            UpdateTilesToTilemap(mapChunks);            
         }
 
         private void GenerateProps(List<ChunkData> mapChunks)
         {
+            //go through chunks
             foreach (var chunk in mapChunks)
             {
                 float distance = 0;
+                //props willbe added to the walkable terrain
                 if(chunk.TileSO.isWalkable)
-                {
+                {                    
                     do
                     {
-                        Sprite propsSprite = chunk.TileSO.propsSprites[Random.Range(0, chunk.TileSO.propsSprites.Length)];                        
+                        //random sprite
+                        Sprite propsSprite = chunk.TileSO.propsSprites[Random.Range(0, chunk.TileSO.propsSprites.Length)];
+                        //get half width of sprite                        
                         float halfSpriteWidth = propsSprite.rect.width / propsSprite.pixelsPerUnit / 2;
+                        //get random X position
                         distance += Random.Range(halfSpriteWidth, halfSpriteWidth * 4);
-                        if(distance > chunk.rect.width - halfSpriteWidth)
-                        {
-                            continue;
-                        }
+                        //this position is too long -> return
+                        if(distance > chunk.rect.width - halfSpriteWidth)continue;   
+                        //get position for props                     
                         Vector3 position = new Vector3(chunk.rect.xMin + distance, chunk.rect.yMax, 0);
-                        GameObject actualProps = Instantiate(propsGO, position, Quaternion.identity);
-                        actualProps.GetComponent<SpriteRenderer>().sprite = propsSprite;
+                        //create and set props game object
+                        GameObject actualProps = Instantiate(propsGO, position, Quaternion.identity, transform);
+                        var v = actualProps.GetComponent<SpriteRenderer>();                        
+                        v.sprite = propsSprite;
+                        //just a bit variation
+                        v.flipX = Random.Range(0f, 1f) > 0.5f ? true : false;
                     } while (distance < chunk.rect.width);
 
                 }
@@ -97,10 +123,10 @@ namespace Generator
         private int GetRandomWidth(TileLevelSO terrainSettings)
         {
             return mapLenght == 0 
-                    ? startChunkLenght 
+                    ? startPlatformLenght 
                     : Random.Range(terrainSettings.minLenght, terrainSettings.maxHeight + 1);
         }
-        private void GenerateBackgroundTiles(List<ChunkData> mapChunks)
+        private void UpdateBackgroundData(List<ChunkData> mapChunks)
         {
             //go through map pick walkable terrain
             //if next or previous chunkdata is not walkable and bigger than this walkable terrain, 
@@ -140,13 +166,14 @@ namespace Generator
             foreach (var item in mapChunks)
             {
                 
-
+                
                 for (int x = item.rect.xMin; x < item.rect.xMax; x++)
                 {
                     for (int y = item.rect.yMin; y < item.rect.yMax; y++)
                     {
+                        var tilemap = item.TileSO.isWalkable ? collisionTilemap : backgroundTilemap;
                         //Debug.Log("saving " + item.TileSO.tileRule + " to the " + collisionTilemap + " at position " + new Vector3Int(x, y));
-                        collisionTilemap.SetTile(new Vector3Int(x, y), item.TileSO.tileRule) ;
+                        tilemap.SetTile(new Vector3Int(x, y), item.TileSO.tileRule) ;
                     }
                     
                 }
@@ -158,7 +185,7 @@ namespace Generator
             
             List<TileLevelSO> chunksToSelect = new();
             //get random terrain
-            foreach (var chunk in chunksForThisMap)
+            foreach (var chunk in tileSO)
             {
                 //mustBeWalkable is for starting purpose and for gap...etc...
                 if(!mustBeWalkable) //not needet to be walkable, get all terrains
